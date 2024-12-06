bbc839a74786dcb16630aa1728d8beb983ab245d
Brandon Jaimes <jaimes1br@gmail.com>
2024-12-06 13:16:32 -0600

    Reto 4 ts

---
+++ retos-ts/decorando_arbol.ts
@@ -0,0 +1,16 @@
+function createXmasTree(height: number, ornament: string): string {
+    let tree: string = '';
+    for (let i = 0; i < height; i++) {
+        let line = '_'.repeat(height - i - 1);
+        let orna = ornament.repeat((2 * i) + 1);
+
+        tree += line + orna + line + '\n';
+    }
+
+    const base: string = '_'.repeat(height - 1);
+
+    tree += base + '#' + base + '\n';
+    tree += base + '#' + base;
+
+    return tree;
+}
