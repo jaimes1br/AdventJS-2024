018c11fc4ee929a7a696128e08b9eb8f21482bec
Brandon Jaimes <jaimes1br@gmail.com>
2024-12-06 17:05:09 -0600

    Reto 5 ts

---
+++ retos-ts/05_emparejando_botas.ts
@@ -0,0 +1,47 @@
+type Shoe = {
+    type: 'I' | 'R'
+    size: number
+}
+
+type tempPair = {
+    [key: number]: {
+        I: number;
+        R: number;
+    };
+};
+
+const shoes: Shoe[] = [
+    { type: 'I', size: 38 },
+    { type: 'R', size: 38 },
+    { type: 'R', size: 42 },
+    { type: 'I', size: 41 },
+    { type: 'I', size: 42 }
+  ]
+  
+console.log(organizeShoes(shoes)); // [38, 42]
+
+function organizeShoes(shoes: Shoe[]): number[] {
+
+    const result: number[] = [];
+    const tmp: tempPair = {};
+
+    for (const { type, size } of shoes){
+        if(!tmp[size]){
+            tmp[size] = { I: 0, R: 0 };
+            tmp[size][type]++;
+        }else{
+            tmp[size][type]++
+        } 
+    }
+
+    for( const [size, { I, R }] of Object.entries(tmp)){
+        const min = Math.min(I,R); 
+        for (let i = 0; i < min; i++) {
+            result.push(+size)
+        }
+    }
+
+    return result
+}
+
+module.exports = organizeShoes;
