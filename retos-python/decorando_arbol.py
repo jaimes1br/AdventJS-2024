4e6883a8d46693c20faacd92609fbca4037a0893
Brandon Jaimes <jaimes1br@gmail.com>
2024-12-06 16:59:10 -0600

    Reto 4 py

---
+++ retos-python/decorando_arbol.py
@@ -0,0 +1,16 @@
+def create_xmas_tree(height, ornament):
+    HASHTAG = '#' 
+    tree = []
+
+    for i in range(height):
+        line = '_' * (height - i - 1)
+        orna = ornament * ((2 * i) + 1)
+        tree.append(line + orna + line)
+
+    
+    base = '_' * (height - 1)
+    
+    tree.append(base + HASHTAG + base)
+    tree.append(base + HASHTAG + base)
+    
+    return '\n'.join(tree)
